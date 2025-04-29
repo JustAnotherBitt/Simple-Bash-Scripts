ba13d69da1def6c232f851dc2011e6c73239a816
Just Another Bit <97632543+JustAnotherBitt@users.noreply.github.com>
2025-04-28 22:43:29 -0300

    Update install-linux-packages.sh

--- install-linux-packages.sh
+++ install-linux-packages.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
 echo "Enter the package name to install:"
 read package
-echo "Installing package $package"
-apt install $package -y
-apt list -a $package
+
+if [ -z "$package" ]; then
+    echo "Package cannot be empty!"
+    exit 1
+fi
+
+echo "Installing package: $package"
+sudo apt install -y "$package"
+apt list -a "$package"
